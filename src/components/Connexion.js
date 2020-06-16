2ece229b353a852d34c8f2b1b3644cbbd464eae1
Kirill <kirill.petrov.centrale@gmail.com>
2020-06-16 19:38:56 +0200

    add redirection if pseudo in input written

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
 class Connnexion extends Component {
+    state = {
+        //Empty by default
+        pseudo: '',
+        goToChat: false
+    }
+
+    handleChange = event => {
+        const pseudo = event.target.value
+        this.setState({pseudo})
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        this.setState({ goToChat: true })
+    }
     render() {
+        if(this.state.goToChat) {
+            return <Redirect to={`/pseudo/${this.state.pseudo}`}/>
+        }
         return(
             <div className="connexionBox">
-                <form className="connexion">
+                <form className="connexion" onSubmit={ this.handleSubmit }>
                     <input
+                        value={this.state.pseudo}
+                        onChange={this.handleChange }
                         placeholder="Pseudo"
                         type="text"
                         required />   
